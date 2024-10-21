65f3b88e644604ebaa6d3d8ae2173a49d05d8952
Алексей Шмелев <Tyuiu.shmelevav@outlook.com>
2024-10-22 00:05:20 +0500

    Task2

---
+++ Tyuiu.ShmelevAV.Sprint2.Task2.V22.Lib/DataService.cs
@@ -0,0 +1,59 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+
+namespace Tyuiu.ShmelevAV.Sprint2.Task2.V22.Lib
+{
+    public class DataService : ISprint2Task2V22
+    {
+        public bool CheckDotInShadedArea(int x, int y)
+        {
+            bool res;
+
+            if ((x >= 3) && (x <= 7) && (y >= 3) && (y <= 5))
+            {
+                res = true;
+            }
+            else if ((x >= 5) && (x <= 7) && (y >= 6) && (y <= 8))
+            {
+                res = true;
+            }
+            else if ((x >= 3) && (x <= 7) && (y >= 9) && (y <= 12))
+            {
+                res = true;
+            }
+            else if ((x >= 8) && (x <= 9) && (y >= 9) && (y <= 10))
+            {
+                res = true;
+            }
+            else if ((x >= 6) && (x <= 8) && (y >= 13) && (y <= 13))
+            {
+                res = true;
+            }
+            else if ((x >= 8) && (x <= 11) && (y >= 12) && (y <= 12))
+            {
+                res = true;
+            }
+            else if ((x >= 8) && (x <= 11) && (y >= 4) && (y <= 6))
+            {
+                res = true;
+            }
+            else if ((x >= 11) && (x <= 11) && (y >= 3) && (y <= 3))
+            {
+                res = true;
+            }
+            else if ((x >= 12) && (x <= 13) && (y >= 5) && (y <= 5))
+            {
+                res = true;
+            }
+            else if ((x >= 14) && (x <= 14) && (y >= 4) && (y <= 10))
+            {
+                res = true;
+            }
+            else
+            {
+                res = false;
+            }
+
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.ShmelevAV.Sprint2.Task2.V22.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+using Tyuiu.ShmelevAV.Sprint2.Task2.V22.Lib;
+
+namespace Tyuiu.ShmelevAV.Sprint2.Task2.V22.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCheckDotInShadedArea()
+        {
+            DataService ds = new DataService();
+            int x = 4;
+            int y = 4;
+
+            bool res = ds.CheckDotInShadedArea(x, y);
+            bool wait = true;
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
